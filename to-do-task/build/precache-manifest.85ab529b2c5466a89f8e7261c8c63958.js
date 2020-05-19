2950eb7cf8a561b2750493f3a3a157462cb9b66c
nil1729 <nilanjan172nsvian@gmail.com>
2020-05-19 15:15:43 +0530

    add build

---
+++ to-do-task/build/precache-manifest.85ab529b2c5466a89f8e7261c8c63958.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6582e19b4cbe6cd1fc07926087d3b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06c0ab1dfa2e46c2bf78",
+    "url": "/static/css/main.7bf9ede2.chunk.css"
+  },
+  {
+    "revision": "3411a0968b648114bf2c",
+    "url": "/static/js/2.23789b7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23789b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c0ab1dfa2e46c2bf78",
+    "url": "/static/js/main.c2e70229.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "1e71c5ad8188e077f379d7964621a7e2",
+    "url": "/static/media/bg.1e71c5ad.jpg"
+  }
+]);
