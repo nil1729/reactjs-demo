e92fbb102298c774030660c8a4b12fdda665aa0b
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-07-02 17:07:11 +0530

    try Production

--- infinite-scroll/server.js
+++ infinite-scroll/server.js
@@ -22,6 +22,13 @@ app.get('/api/photos', (req, res) => {
 		});
 });
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
