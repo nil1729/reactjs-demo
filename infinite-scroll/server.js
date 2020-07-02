1e59012f2f112177a7588e3bf2f66464787a233a
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-07-02 17:05:50 +0530

    add ENV

--- infinite-scroll/server.js
+++ infinite-scroll/server.js
@@ -1,5 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 const fetch = require('node-fetch');
-const config = require('config');
 const express = require('express');
 const Unsplash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
@@ -7,20 +9,21 @@ const app = express();
 
 global.fetch = fetch;
 
-const unsplash = new Unsplash({ accessKey: config.get('AccessKey') });
-
-
-app.get('/api/photos', (req, res) => {
-    unsplash.search.photos("technology", req.query.start, 30)
-    .then(toJson)
-    .then(json => {        
-        res.json(json);
-    });
+const unsplash = new Unsplash({
+	accessKey: process.env.ACCESS_KEY,
 });
 
+app.get('/api/photos', (req, res) => {
+	unsplash.search
+		.photos('technology', req.query.start, 30)
+		.then(toJson)
+		.then(json => {
+			res.json(json);
+		});
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+	console.log(`Server started on port ${PORT}`);
 });
