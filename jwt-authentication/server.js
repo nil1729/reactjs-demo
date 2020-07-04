d5d139a54ee10e1604b9833b9813f0d844c0aa73
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2020-07-04 21:21:01 +0530

    add production

--- jwt-authentication/server.js
+++ jwt-authentication/server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const isAuth = require('./middleware/isAuth');
-
+const path = require('path');
 
 //! make API Cross Site accessible for Development
 if (process.env.NODE_ENV !== 'production') {
@@ -38,6 +38,12 @@ app.use('/api_v2', graphqlHTTP({
     graphiql: process.env.NODE_ENV === 'production' ? false : true
 }));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 //! Server Port Setup
 const PORT = process.env.PORT;
